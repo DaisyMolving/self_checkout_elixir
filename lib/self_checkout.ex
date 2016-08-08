eda0e832f0bbe5b25dd9dd902c9dc91ec1c89191
Daisy Molving <daisymolving@gmail.com>
2016-08-08 16:48:05 +0100

    wrote and refactored tests for input and outputs, wrote function to convert to number by extracting number

--- lib/self_checkout.ex
+++ lib/self_checkout.ex
@@ -1,6 +1,17 @@
 defmodule SelfCheckout do
 
   def request_item_price do
-    IO.gets("Enter the price of item")
+    IO.gets("Enter the price of item: ")
   end
+
+  def request_item_quantity do
+    IO.gets("Enter the quantity of item: ")
+  end
+
+  def convert_to_num(string) do
+    Integer.parse(string) |> extract_number
+  end
+
+  defp extract_number({num, _rem}), do: num
+  defp extract_number(:error), do: :invalid
 end
--- test/self_checkout_test.exs
+++ test/self_checkout_test.exs
@@ -3,17 +3,50 @@ defmodule SelfCheckoutTest do
   use ExUnit.Case
 
   test "displays message to ask for price of item" do
-    result = capture_io(fn ->
-      SelfCheckout.request_item_price
-    end)
-    assert result == "Enter the price of item"
+    func = &SelfCheckout.request_item_price/0
+    result = capture_output(func)
+  assert result == "Enter the price of item: "
   end
 
   test "retrieves user input for the price of an item" do
-    result = capture_io([input: "1", capture_prompt: false], fn ->
-      value = SelfCheckout.request_item_price
+    input = "1"
+    func = &SelfCheckout.request_item_price/0
+    result = capture_user_input(input, func)
+    assert result == "1"
+  end
+
+  test "displays message to ask for quantity of item" do
+    func = &SelfCheckout.request_item_quantity/0
+    result = capture_output(func)
+    assert result == "Enter the quantity of item: "
+  end
+
+  test "retrieves user input for the quantity of an item" do
+    input = "2"
+    func = &SelfCheckout.request_item_quantity/0
+    result = capture_user_input(input, func)
+    assert result == "2"
+  end
+
+  test "converts a string to a numeric value" do
+    assert SelfCheckout.convert_to_num("1") == 1
+    assert SelfCheckout.convert_to_num("5") == 5
+  end
+
+  test "validates user input when converting to a number" do
+    assert SelfCheckout.convert_to_num("blah") == :invalid
+  end
+
+  defp capture_user_input(input, func) do
+    capture_io([input: input, capture_prompt: false], fn ->
+      value = func.()
       IO.write(value)
     end)
-    assert result == "1"
+  end
+
+  defp capture_output(func) do
+    capture_io(fn ->
+      func.()
+    end)
   end
 end
